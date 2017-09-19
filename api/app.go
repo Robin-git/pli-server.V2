833073763f2d403925e450ab29f2d29daec8b116
Robin GUERIN <robin.guerin@numen.fr>
2017-09-19 15:29:14 +0200

    refactoring

--- api/app.go
+++ api/app.go
@@ -1,40 +1,10 @@
 package api
 
 import (
-	"fmt"
 	"gloo-server/controllers"
-	"gloo-server/models"
-	"log"
-	"os"
 
 	"github.com/gorilla/mux"
 )
-
-// SQLConnection is type of connection
-type SQLConnection struct {
-	Login    string
-	Password string
-	Database string
-}
-
-// GetConnection return service whith connection to sql database
-func (c *SQLConnection) GetConnection() *models.Service {
-	var env string
-	switch os.Getenv("GLOO_ENV") {
-	case "dev":
-		env = fmt.Sprintf("%s_dev", c.Database)
-		log.Println("Started on dev")
-	case "prod":
-		env = fmt.Sprintf("%s_prod", c.Database)
-		log.Println("Started on prod")
-	default:
-		env = fmt.Sprintf("%s_dev", c.Database)
-		log.Println("Warning : No GLOO_ENV selected")
-		log.Println("Default --> Started on dev")
-	}
-	conn := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", c.Login, c.Password, env)
-	return models.InitMariaDB(conn)
-}
 
 // NewRouter return new mux Router
 func NewRouter() *mux.Router {
---
+++ api/env.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+	"gloo-server/models"
+	"log"
+	"os"
+)
+
+// SQLConnection is type of connection
+type SQLConnection struct {
+	Login    string
+	Password string
+	Database string
+}
+
+// GetConnection return service whith connection to sql database
+func (c *SQLConnection) GetConnection() *models.Service {
+	var env string
+	switch os.Getenv("GLOO_ENV") {
+	case "dev":
+		env = fmt.Sprintf("%s_dev", c.Database)
+		log.Println("Started on development")
+	case "rec":
+		env = fmt.Sprintf("%s_rec", c.Database)
+		log.Println("Started on recipe")
+	case "prod":
+		env = fmt.Sprintf("%s_prod", c.Database)
+		log.Println("Started on production")
+	default:
+		env = fmt.Sprintf("%s_dev", c.Database)
+		log.Println("Warning : No GLOO_ENV selected")
+		log.Println("Default --> Started on development")
+	}
+	conn := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", c.Login, c.Password, env)
+	return models.InitMariaDB(conn)
+}
